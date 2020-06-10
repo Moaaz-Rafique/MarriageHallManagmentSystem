ba3aee0c9c11d461b8c32621a5fc3d484531eaaf
Moaaz-Rafique <moaazrafiquetk@gmail.com>
2020-06-10 19:16:26 +0500

    Dashboard

--- src/oopproject/MarriageHall.java
+++ src/oopproject/MarriageHall.java
@@ -1,60 +1,59 @@
 package oopproject;
+
 import java.util.ArrayList;
+
 public class MarriageHall {
-    
-private String Name,Price;
-private int CapacityPeople,CapacitySize;
-private ArrayList<String> Dates = new ArrayList<>();
 
+    private String name, price;
+    private int capacityPeople, capacitySize;
+    private ArrayList<String> dates = new ArrayList<>();
 
-public MarriageHall(String Name,String Price,int CapacityP,int CapacityS,String D){
-this.Name = Name;
-this.Price = Price;
-this.CapacityPeople = CapacityP;
-this.CapacitySize = CapacityS;
-this.Dates.add(D);
-}
+    public MarriageHall(String Name, String Price, int CapacityP, int CapacityS, String D) {
+        this.name = Name;
+        this.price = Price;
+        this.capacityPeople = CapacityP;
+        this.capacitySize = CapacityS;
+        this.dates.add(D);
+    }
 
-public void SetName(String N){
-this.Name = N;
-}
+    public void SetName(String N) {
+        this.name = N;
+    }
 
-public void SetPrice(String P){
-this.Price = P;
-}
+    public void SetPrice(String P) {
+        this.price = P;
+    }
 
-public void SetCapacityPeople(int CP){
-this.CapacityPeople = CP;
-}
+    public void SetCapacityPeople(int CP) {
+        this.capacityPeople = CP;
+    }
 
-public void SetCapacitySize(int CS){
-this.CapacitySize = CS;
-}
+    public void SetCapacitySize(int CS) {
+        this.capacitySize = CS;
+    }
 
-public void SetDate(String D){
-this.Dates.add(D);
-}
+    public void SetDate(String D) {
+        this.dates.add(D);
+    }
 
-public String GetName(){
-return this.Name;
-}
+    public String GetName() {
+        return this.name;
+    }
 
-public String GetPrice(){
-return this.Price;
-}
+    public String GetPrice() {
+        return this.price;
+    }
 
-public int SetCapacityPeople(){
-return this.CapacityPeople;
-}
+    public int SetCapacityPeople() {
+        return this.capacityPeople;
+    }
 
-public int SetCapacitySize(){
-return this.CapacitySize;
-}
+    public int SetCapacitySize() {
+        return this.capacitySize;
+    }
 
-public ArrayList<String> SetDate(){
-return Dates;
-}
-
+    public ArrayList<String> SetDate() {
+        return dates;
+    }
 
 }
-
